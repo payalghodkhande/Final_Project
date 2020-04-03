d3fe9aea039865fe87b6913ab3699e767f765dfc
payalghodkhande <sanhalpayal@gmail.com>
2020-04-02 19:46:55 -0500

    Final

--- Logic/AuthorLogic.cs
+++ Logic/AuthorLogic.cs
@@ -8,11 +8,36 @@ using System.Text;
 
 namespace Logic
 {
-    public class AuthorLogic : Baselogic<Author>
+    public class AuthorLogic : BaseLogic<Author>
     {
         public AuthorLogic(IDataRepository<Author> repository) : base(repository)
         {
+           
+        }
 
+        protected override void Verify(Author[] pocos)
+        {
+            foreach (Author item in pocos)
+            {
+                if (string.IsNullOrEmpty(item.FirstName))
+                {
+                    throw new AggregateException("Error");
+                }
+            }
+        }
+
+
+        public override void Add(Author[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(Author[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
         }
     }
+
 }
--- Logic/BookLogic.cs
+++ Logic/BookLogic.cs
@@ -11,7 +11,30 @@ namespace Logic
         public BookLogic(IDataRepository<Book> repository) : base(repository)
         {
 
+        }
 
+        protected override void Verify(Book[] pocos)
+        {
+            foreach (Book item in pocos)
+            {
+                if (string.IsNullOrEmpty(item.Title))
+                {
+                    throw new AggregateException("Error");
+                }
+            }
+        }
+
+
+        public override void Add(Book[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(Book[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
         }
     }
 }
